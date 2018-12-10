1ef7abfa5fa80e9bbfea85e9c283cdaf5fb29ad7
Emily Dalton <emilydalton@dhcp-10-105-205-249.wireless.northwestern.private>
2018-12-10 13:31:38 -0600

    correcting rotten tomatoes dot notation in movie-this and making it work overall

--- liri.js
+++ liri.js
@@ -75,55 +75,47 @@ function spotify (){
 // console.log("I am the whatitSays function")
 // }
 
-// function movie(){
-// // const movie = () => {
-// // Take a move with multiple words (ex: Forrest Gump) as a Node argument and retrieve the year it was created.
+function movie(){
+// const movie = () => {
+// Take a move with multiple words (ex: Forrest Gump) as a Node argument and retrieve the year it was created.
 
-// // Store all of the arguments in an array
-// var nodeArgs = process.argv;
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
 
-// // Create an empty variable for holding the movie name
-// var movieName = "";
+// Create an empty variable for holding the movie name
+var movieName = "";
 
-// // Loop through all the words in the node argument
-// // And do a little for-loop magic to handle the inclusion of "+"s
-// for (var i = 2; i < nodeArgs.length; i++) {
-
-//   if (i > 2 && i < nodeArgs.length) {
-//     movieName = movieName + "+" + nodeArgs[i];
-//   }
-//   else {
-//     movieName += nodeArgs[i];
-
-//   }
-// }
-
-// // Then run a request with axios to the OMDB API with the movie specified
-// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-// // This line is just to help us debug against the actual URL.
-// console.log(queryUrl);
-
-// axios.get(queryUrl).then(
-//   function(response) {
-
-//     //movie title
-//     console.log("Title: " + response.data.Title);
-//     // release year 
-//     console.log("Release Year: " + response.data.Year);
-//     //IMDB Rating
-//     console.log("IMDB Rating: "  + response.data.imdbRating);
-//     //Rotten Tomatoes Rating of the movie.
-//     console.log("Rotten Tomatoes Rating:" + response.data.Ratings);
-//     //Country where the movie was produced.
-//     console.log("Country of Production: " + response.data.Country);
-//     //Language of the movie.
-//     console.log("Language: " + response.data.Language);
-//     // Plot of the movie.
-//     console.log("Plot: " + response.data.Plot);
-//     //Actors in the movie.
-//     console.log("Actors: " + response.data.Actors);
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+movieName=process.argv.slice(2).join(" ");
 
 
-//   });
-// }
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+
+    //movie title
+    console.log("\nTitle: " + response.data.Title);
+    // release year 
+    console.log("\nRelease Year: " + response.data.Year);
+    //IMDB Rating
+    console.log("\nIMDB Rating: "  + response.data.imdbRating);
+    //Rotten Tomatoes Rating of the movie.
+    console.log("\nRotten Tomatoes Rating:" + response.data.Ratings[1].Value);
+    //Country where the movie was produced.
+    console.log("\nCountry of Production: " + response.data.Country);
+    //Language of the movie.
+    console.log("\nLanguage: " + response.data.Language);
+    // Plot of the movie.
+    console.log("\nPlot: " + response.data.Plot);
+    //Actors in the movie.
+    console.log("\nActors: " + response.data.Actors);
+
+
+  });
+}
